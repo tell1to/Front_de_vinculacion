f80d8fd25d74191128fa4e346eb248936c5d1a6f
OmarJB20 <ojc.santillan@yavirac.edu.ec>
2025-01-19 17:51:02 -0500

    19/01/25

--- src/app/demo/components/pages/cita/cita.component.ts
+++ src/app/demo/components/pages/cita/cita.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import axios from 'axios'
 
 @Component({
   selector: 'app-cita',
-  standalone: true,
-  imports: [],
   templateUrl: './cita.component.html',
   styleUrl: './cita.component.scss'
 })
 export class CitaComponent {
+  private apiUrl = environment.api
+  medicos: any[] = []; // Cambiar a tipo 'any[]' si necesitas propiedades adicionales como 'id'
+  paciente: any;
+  cedula: string = '';
+  telefono: string = '';
+  fecha: string = '';
+  descripcion: string = '';
+  correoElectronico: string = '';
+  medicoSeleccionado: string = '';
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // Cargar los médicos al iniciar el componente
+    this.mostrarMedicos();
+  }
+
+  getDoctors(): Observable<any> {
+    const response = axios.get(`${this.apiUrl}/doctors`);
+    return from(response.then(res => res.data));
+  }
+  getPatients(id:string): Observable<any> {
+  const response = axios.get(`${this.apiUrl}/patients/${id}`);
+  return from(response.then(res => res.data));
+  }
+
+  mostrarMedicos(): void {
+    this.getDoctors().subscribe(
+      (doctors: any[]) => {
+        this.medicos = doctors;
+        console.log(doctors)
+      },
+      error => {
+        console.error('Error al obtener los médicos:', error);
+      }
+    );
+  }
+  // Método para agregar la cita
+  agregarCita(): void {
+    const citaData = {
+      cedula: this.cedula,
+      telefono: this.telefono,
+      fecha: this.fecha,
+      descripcion: this.descripcion,
+      correoElectronico: this.correoElectronico,
+      medicoId: this.medicoSeleccionado
+    };
+  }
 }
--- src/app/demo/components/pages/cita/cita.module.ts
+++ src/app/demo/components/pages/cita/cita.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CitaRoutingModule } from './cita-routing.module';
+import { FormsModule } from '@angular/forms';
+import { CitaComponent } from './cita.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CitaComponent],
   imports: [
     CommonModule,
-    CitaRoutingModule
+    CitaRoutingModule,
+    FormsModule
   ]
 })
 export class CitaModule { }
